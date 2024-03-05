09c021601781ed572236e1abd98e7daf0c3099b8
Jacob Williams <jacob.t.williams@outlook.com>
2024-03-05 14:54:55 -0600

    Initial Commit

---
+++ src/main/java/novamachina/exnjei/jei/sifting/WetSiftingRecipeCategory.java
@@ -0,0 +1,32 @@
+package novamachina.exnjei.jei.sifting;
+
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.RecipeType;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import novamachina.exnihilosequentia.common.utility.ExNihiloConstants;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nonnull;
+
+public class WetSiftingRecipeCategory extends AbstractSieveRecipeCategory {
+
+  @Nonnull
+  public static final ResourceLocation UID =
+      new ResourceLocation(ExNihiloConstants.ModIds.EX_NIHILO_SEQUENTIA, "wet_sifting");
+
+  public WetSiftingRecipeCategory(@NotNull IGuiHelper guiHelper) {
+    super(guiHelper, true);
+  }
+
+  @Override
+  public RecipeType<JEISieveRecipe> getRecipeType() {
+    return new RecipeType<>(UID, JEISieveRecipe.class);
+  }
+
+  @Nonnull
+  @Override
+  public Component getTitle() {
+    return Component.translatable("jei.category.wet_sifting");
+  }
+}
